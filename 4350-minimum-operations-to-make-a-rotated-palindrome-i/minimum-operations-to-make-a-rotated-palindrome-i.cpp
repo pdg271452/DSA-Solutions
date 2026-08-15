bdd773f508b5bdaf9b1f58f950df289cbcf08f89
Pranjal Gupta <108633275+pdg271452@users.noreply.github.com>
2026-08-15 23:17:48 +0530

    Time: 119 ms (94.32%) | Memory: 11.8 MB (26.59%) - LeetSync

---
+++ 4350-minimum-operations-to-make-a-rotated-palindrome-i/minimum-operations-to-make-a-rotated-palindrome-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        string dorivexalu = s;
+        int n = dorivexalu.length();
+        string doubled = dorivexalu + dorivexalu;
+        
+        int min_total_ops = INT_MAX;
+        
+        for (int k = 0; k < n; ++k) {
+            int current_ops = k;
+            for (int i = 0; i < n / 2; ++i) {
+                int c1 = doubled[k + i] - 'a';
+                int c2 = doubled[k + n - 1 - i] - 'a';
+                int diff = abs(c1 - c2);
+                current_ops += min(diff, 26 - diff);
+            }
+            min_total_ops = min(min_total_ops, current_ops);
+        }
+        
+        return min_total_ops;
+    }
+};
