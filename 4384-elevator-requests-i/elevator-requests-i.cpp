b058d60ff7acbc84c0b6be6f9ddd49c4248d07de
Pranjal Gupta <108633275+pdg271452@users.noreply.github.com>
2026-08-16 01:09:07 +0530

    Time: 0 ms (100.00%) | Memory: 31 MB (33.33%) - LeetSync

--- 4384-elevator-requests-i/elevator-requests-i.cpp
+++ 4384-elevator-requests-i/elevator-requests-i.cpp
@@ -1,14 +1,12 @@
-class Solution {
+class Solution{
 public:
-    int elevatorRequests(int n, vector<int>& requests) {
-        int totalTime = 0;
-        int currentFloor = 0;
-        
-        for (int floor : requests) {
-            totalTime += abs(floor - currentFloor);
-            currentFloor = floor;
-        }
-        
-        return totalTime;
-    }
+int elevatorRequests(int n, vector<int>& requests){
+int totalTime = 0;
+int currentFloor = 0;
+for(int floor: requests) {
+totalTime += abs(floor - currentFloor);
+currentFloor = floor;
+}
+return totalTime;
+}
 };
